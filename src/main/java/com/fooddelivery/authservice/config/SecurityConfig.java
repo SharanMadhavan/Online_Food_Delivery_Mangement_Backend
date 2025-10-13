4ad6749584044bc1f11c77ab0140f399b99056b9
Sharanmadhavan <sharanmadhavan@gmail.com>
2025-10-13 23:34:24 +0530

    allowing vercel deployment request url

--- src/main/java/com/fooddelivery/authservice/config/SecurityConfig.java
+++ src/main/java/com/fooddelivery/authservice/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080")); // frontend origin
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080","https://food-one-ashen.vercel.app")); // frontend origin
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
